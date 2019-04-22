d0e2d25799b2d593e0065b53189ff3fdbd351d8b
Ziv Friedman <ziv.friedman@gmail.com>
2019-04-23 00:23:13 +0300

    listener-transmitter and receiver-visualizer added

---
+++ src/baby_monitor_listener_transmitter/baby_monitor_listener_transmitter.ino
@@ -0,0 +1,87 @@
+/************************************************************************************************* 
+ *  About monitor:
+ *  The main purpose is to leave the decision about indication to the receiver component
+ *  The secondary purpose is to refrain from unneccessary transmissions
+ *  Hence, a reasonable threshold is set for transmission, and above it everything is 
+ *  sen as-is
+ *  
+ *  Audio collection method:
+ *  (Max-Min) over [sample_time] are collected as [amp], and aggregated over [collect_time]
+ *  to an array [amp_samp]
+ *  
+ *  If any of the [amp] values exceeds the [min_amp_threshold], a transmission is invoked
+ *  with the sum of the [amp_samp] values
+ *  
+ *  Time of last transmission is saved to [last_transmission_time] and if more than 10 sec
+ *  passed from last transmission, a 'zero-amp' transmission is invoked as a 'keep-alive'
+ *  signal
+ ************************************************************************************************/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// pin data
+const int MY_LED = 3; // debug only
+const int MIC_PIN = A1;
+
+// mic data
+const int sampleTime = 50;
+int micOut;
+int min_amp_threshold = 10;
+unsigned long last_transmission_time = 0;
+unsigned long keepalive_interval = 2000;
+
+// radio data
+RF24 radio(9,10); // CE, CSN
+const byte address[6] = "00023";
+
+const bool OUTPUT_TO_SERIAL = false;
+
+void setup() {
+  if (OUTPUT_TO_SERIAL) { Serial.begin(9600); }
+  setup_mic();
+  setup_radio();
+}
+
+void loop() {
+  float sample = (findPTPAmp()+findPTPAmp()+findPTPAmp()+findPTPAmp()) / 4;
+  transmit(sample);
+}
+
+void transmit(float mic_data) {
+  if (OUTPUT_TO_SERIAL) { Serial.println(mic_data); }
+  radio.write(&mic_data, sizeof(mic_data));
+  last_transmission_time = millis();
+}
+
+void setup_mic() {
+  pinMode(MIC_PIN, INPUT);
+  last_transmission_time = millis();
+}
+
+void setup_radio() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+float findPTPAmp(){
+   unsigned long startTime = millis();  // Start of sample window
+   unsigned int PTPAmp = 0; 
+   unsigned int maxAmp = 0;
+   unsigned int minAmp = 1023;
+
+   while(millis() - startTime < sampleTime) 
+   {
+      micOut = analogRead(MIC_PIN);
+      if( micOut < 1023)
+      {
+        if (micOut > maxAmp) { maxAmp = micOut; }
+        else if (micOut < minAmp) { minAmp = micOut; }
+      }
+   }
+  PTPAmp = maxAmp - minAmp;
+  return PTPAmp / 10.0;
+}
---
+++ src/baby_monitor_visualizer_recevier/baby_monitor_visualizer_recevier.ino
@@ -0,0 +1,156 @@
+ /*********************************************************************************************
+ * About visualizer-receiver:
+ * The main purpose is to display alarm whenever threshold was crossed. 
+ * The seconday purpose is to visualize on screen the last SCREEN_WIDTH samples received
+ * 
+ * Data received is the average of some collected samples on listener, with 
+ * at least one sample over the 'quiet thershold' (the minimal amplitude that is 
+ * considered 'not-quiet')
+ * 
+ * Visualizer shows all received samples on screen, and user is able to determine from what
+ * noise level and up to display the visual-alarm
+ *********************************************************************************************/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// radio data
+RF24 radio(9,10); // CE, CSN
+const byte address[6] = "00023";
+
+// OLED data
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+const int SCREEN_WIDTH = 128;
+const int SCREEN_HEIGHT = 32;
+
+// monitor related data
+float wave_samples[SCREEN_WIDTH];
+unsigned long last_data_time = millis();
+int noise_threshold = 20;
+
+const int ALARM_LEDS = 5;
+unsigned long show_alarm_until = 0;
+
+int last_potentiometer_value = 0;
+unsigned long next_potentiometer_read = 0;
+
+
+const bool OUTPUT_TO_SERIAL = true;
+
+void setup() {
+  pinMode(ALARM_LEDS, OUTPUT);
+  digitalWrite(ALARM_LEDS, LOW);
+  
+  if (OUTPUT_TO_SERIAL) { 
+    Serial.begin(9600); 
+    for (int i=0; i<100; i++) { Serial.println(10); }
+  }
+  setup_radio();
+  setup_oled();
+}
+
+void loop() {
+  Serial.println(get_radio_sample());
+  delay(10);
+}
+
+void no_loop() {
+  digitalWrite(ALARM_LEDS, (show_alarm_until > millis()) ? HIGH : LOW);
+
+  int encoder_value = readPotentiometer();
+  if (noise_threshold != encoder_value) {
+    noise_threshold = encoder_value;
+    display_samples();
+  }
+  
+  float sample_from_radio = get_radio_sample();
+
+  if (sample_from_radio > noise_threshold) {
+    show_alarm_until = millis() + 1000;
+  }
+  
+  if (sample_from_radio >= 0) {
+    add_sample(sample_from_radio);
+    display_samples();
+    if (OUTPUT_TO_SERIAL) { Serial.println(sample_from_radio); }
+  }
+  else if (millis() - last_data_time > 60 * 1000) {
+    // if last transmission was more than a minute ago -
+    // it means we have a communication problem
+    if (OUTPUT_TO_SERIAL) { Serial.println("No comm in the last 60 seconds"); }
+    delay(100);
+  }
+}
+
+float get_radio_sample() {
+  float sample = -1; // fallback for no data available
+  if (radio.available()) {    
+    radio.read(&sample, sizeof(sample));
+  }
+  return sample;
+}
+
+void add_sample(float samp) {
+  for (int i=127; i>0; i--) {
+    wave_samples[i] = wave_samples[i-1];
+  }
+  wave_samples[0] = samp;
+}
+
+void display_samples() {
+  display.clearDisplay();
+  
+  // minimum is always 0, find maximum and scale is relative to SCREEN_HEIGHT
+  float samp_max = 0;
+  for (int i=0; i<SCREEN_WIDTH; i++) {
+    samp_max = max(wave_samples[i], samp_max);
+  }
+
+  float scale = (float)SCREEN_HEIGHT / samp_max;
+  
+  for (int i=0; i<SCREEN_WIDTH; i++) {
+    int val = (int)(wave_samples[i]*scale);
+    display.drawFastVLine(i, 0, val, WHITE);
+  }
+
+  //noise_threshold
+  display.drawFastHLine(0, noise_threshold*scale, SCREEN_WIDTH, WHITE);
+  
+  display.display();
+}
+
+void setup_oled() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+}
+
+void setup_radio() {
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+int readPotentiometer() {
+  if (millis() < next_potentiometer_read) {
+    return last_potentiometer_value;
+  }
+  
+  int val = 999;
+  for (int i=0; i<97; i++) {
+    val = min(val, analogRead(A2));
+  }
+
+  val = 10.0 * (7.0 - log(val + 1));
+
+  next_potentiometer_read = millis() + 300;
+  last_potentiometer_value = val;
+  return val;
+}
+
